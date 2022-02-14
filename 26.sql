9e462629e20101aca3cced083f378c737e0c05d9
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-02-14 14:04:00 +0100

    Update 26.sql

--- 26.sql
+++ 26.sql
@@ -2,12 +2,11 @@ SELECT * FROM (
     SELECT `nombre_empleado`, `salario` + `comision` as `salario_total`
     FROM `empleados`
     WHERE `comision` IS NOT NULL
-    ORDER BY `salario_total`
 ) a
 UNION
 SELECT * FROM (
     SELECT `nombre_empleado`, `salario` as `salario_total`
     FROM `empleados`
     WHERE `comision` IS NULL
-    ORDER BY `salario_total`
 ) b
+ORDER BY `salario_total`
